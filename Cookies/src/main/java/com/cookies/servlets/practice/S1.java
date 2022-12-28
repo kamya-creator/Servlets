a1c14ba2fd0a35eb27ad91b162c08f36f03b8a43
kamya-creator <71540845+kamya-creator@users.noreply.github.com>
2022-12-28 23:55:04 +0530

    Add files via upload

---
+++ Cookies/src/main/java/com/cookies/servlets/practice/S1.java
@@ -0,0 +1,50 @@
+package com.cookies.servlets.practice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class S1
+ */
+@WebServlet("/s1")
+public class S1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		out.print("<h1> Hello " + name + "</h1>");
+		
+		// Adding new Servlet here
+		
+		RequestDispatcher rd = request.getRequestDispatcher("s2");
+		out.print("<a href ="+"s2"+">Click here to go Servlet 2</a>");
+		//rd.forward(request, response);
+		
+		
+		Cookie cookie = new Cookie("user", name);
+		response.addCookie(cookie);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Cookies/src/main/java/com/cookies/servlets/practice/S2.java
@@ -0,0 +1,47 @@
+package com.cookies.servlets.practice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class S2
+ */
+@WebServlet("/s2")
+public class S2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		out.print("Servlet 2" );
+		Cookie c[] = request.getCookies();
+		String n = c[0].getValue();
+		out.print("<h1>"+n+"</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
